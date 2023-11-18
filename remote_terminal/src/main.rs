23f68bd8fe18623a02c5ba6d9d7910c73f912345
cristian_david_gentoo <cristian.pacheco@correounivalle.edu.co>
2023-11-18 15:44:40 -0500

    IMplemeting the the basic args recovery, it with the dependency clap

--- remote_terminal/src/main.rs
+++ remote_terminal/src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, remote shell!");
+
+    let app = Command::new("Shell remote")
+                .version("0.0.1")
+                .about("Client-Server remote shell")
+                .author("Cristian Pacheco, Julian Puyo, Sebastian Molina");
+
+    let first_name = Arg::new("command")
+                            .long("f")
+                            .help("Command to exectue in the remote shell")
+                            .required(true);
+    
+    let last_name = Arg::new("address")
+                            .long("l")
+                            .help("Address of remote shell")
+                            .required(true);
+
+    let age = Arg::new("port")
+                    .long("a")
+                    .help("Port of the server")
+                    .required(true);
+
+    let app = app.arg(first_name).arg(last_name).arg(age);
+
+    let matches = app.get_matches();
+    let command = matches.get_one::<String>("command").unwrap();
+    let address = matches.get_one::<String>("address").unwrap();
+    let port: &i8 = matches.get_one::<i8>("port").unwrap();
+    
+    println!("{:?}", command); 
+    println!("{:?}", address);
+    println!("{:?}", port);
+                    
 }
+
+// matches.get_many::<String>("arg").unwrap().collect::<Vec<_>>(),
